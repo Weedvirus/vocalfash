5446f3b20e62d7dbe4a1a9081ee887212d476a50
Weedvirus <142642629+Weedvirus@users.noreply.github.com>
2026-06-27 05:09:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
 
-TOKEN = "8900156068:AAHoGMlyeiwhk5FLdcLZoOBZOTR6W8Ppt_0"
+TOKEN = "8900156068:AAEO_dXnY1RvXyxWyru7K1OfO1PUNvpP_kI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "سلام 🎵\nبات با موفقیت روشن شد!"
+        "🎵 سلام! یک فایل صوتی بفرست."
+    )
+
+async def get_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "✅ فایل با موفقیت دریافت شد!"
     )
 
 def main():
     app = Application.builder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        MessageHandler(
+            filters.AUDIO | filters.Document.AUDIO,
+            get_audio
+        )
+    )
 
     print("Bot Started...")
     app.run_polling()
